c69ded01680b2c78be33994466664248d239c23f
Hector <141383937+hfelix3@users.noreply.github.com>
2024-01-09 18:30:10 -0700

    changed file name in controllers to homeRoutes and in serverjs in root

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-
-const commentRoutes = require('./commentRoutes');
 const userRoutes = require('./userRoutes');
+const commentRoutes = require('./commentRoutes');
+// const homeRoutes = require('../homeRoutes');
 
-router.use('/api', commentRoutes);
 router.use('/users', userRoutes);
+router.use('/comment', commentRoutes);
+// router.use('/IronBlog', homeRoutes);
 
 module.exports = router;
