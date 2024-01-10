026e535501f9f6bf16d499d94d4574581da93b0a
Hector <141383937+hfelix3@users.noreply.github.com>
2024-01-09 21:20:27 -0700

    added log out file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-var session = require('express-session')
+// load dependencies
+var express = require("express");
+var Sequelize = require("sequelize");
+var session = require("express-session");
 const routes = require('./controllers');
 const path = require('path')
 
@@ -9,6 +11,8 @@ const hbs = exphbs.create({});
 
 // initialize application
 const app = express();
+// initialize sequelize with session store
+var SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 // Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
@@ -21,6 +25,26 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// ?DO I NEED THIS? GOT IT FROM CONNECT-SESSION-SEQUELIZE INSTALL
+// // create database, ensure 'sqlite3' in your package.json
+// var sequelize = new Sequelize("database", "username", "password", {
+//   dialect: "sqlite",
+//   storage: "./session.sqlite",
+// });
+
+// // configure express
+// var app = express();
+// app.use(
+//   session({
+//     secret: "keyboard cat",
+//     store: new SequelizeStore({
+//       db: sequelize,
+//     }),
+//     resave: false, // we support the touch method so per the express-session docs this should be set to false
+//     proxy: true, // if you do SSL outside of node.
+//   })
+// );
 
 app.use(session({
   secret: 'keyboard cat',
